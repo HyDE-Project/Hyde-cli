fb2986c6ec150ece803428358c8393a029ec9b37
kRHYME7 <alampayk@gmail.com>
2024-05-02 02:35:19 +0800

    Hyde power toggle

--- shell-completions/Hyde.bash
+++ shell-completions/Hyde.bash
@@ -7,28 +7,28 @@ _Hyde () {
     local words cword
     _get_comp_words_by_ref -n "$COMP_WORDBREAKS" words cword
 
-    local -a literals=("binds" "game" "::=" "version" "all" "update" "wallpaper" "shell" "0" "Link" "reload" "--blur" "restore" "revert" "cache" "list" "prev" "3" "control" "next" "--opacity" "monitors" "save" "unset" "power" "next" "1" "2" "Config" "systeminfo" "--borderangle" "BackUp" "mode" "theme" "branch" "wallbash" "prev" "backup" "reset" "events" "select" "toggle" "--opacity" "glyph" "set" "patch" "--borderangle" "emoji" "pastebin" "set" "clean" "rebuild" "--animations" "inject" "screencap" "select" "screencap" "reload" "sddm" "Clone" "theme" "show" "select" "waybar" "--animations" "upgrade" "run" "chaotic" "--blur" "man")
+    local -a literals=("binds" "game" "::=" "version" "all" "update" "wallpaper" "shell" "0" "Link" "reload" "--blur" "restore" "revert" "cache" "--opacity" "list" "prev" "3" "next" "control" "monitors" "save" "unset" "power" "next" "1" "2" "Config" "systeminfo" "--borderangle" "BackUp" "mode" "theme" "branch" "wallbash" "prev" "backup" "reset" "events" "select" "toggle" "--opacity" "glyph" "set" "patch" "--borderangle" "emoji" "pastebin" "set" "toggle" "clean" "rebuild" "--animations" "inject" "screencap" "select" "screencap" "reload" "sddm" "Clone" "theme" "show" "select" "waybar" "--animations" "upgrade" "run" "chaotic" "--blur" "man")
 
     declare -A literal_transitions
-    literal_transitions[0]="([53]=1 [3]=3 [24]=4 [58]=5 [5]=3 [60]=6 [6]=7 [29]=3 [61]=8 [7]=9 [63]=10 [65]=3 [10]=3 [34]=3 [66]=11 [35]=12 [12]=13 [37]=14 [14]=15)"
-    literal_transitions[1]="([67]=3)"
-    literal_transitions[4]="([23]=20 [38]=3 [1]=3 [22]=18)"
+    literal_transitions[0]="([54]=1 [3]=3 [24]=4 [59]=5 [5]=3 [61]=6 [6]=7 [29]=3 [62]=8 [7]=9 [64]=10 [66]=3 [10]=3 [34]=3 [67]=11 [35]=12 [12]=13 [37]=14 [14]=15)"
+    literal_transitions[1]="([68]=3)"
+    literal_transitions[4]="([23]=20 [38]=3 [1]=3 [50]=3 [22]=18)"
     literal_transitions[5]="([33]=3)"
-    literal_transitions[6]="([55]=3 [19]=3 [36]=3 [44]=3 [45]=3)"
-    literal_transitions[7]="([49]=3 [62]=3 [25]=3 [16]=3)"
-    literal_transitions[8]="([21]=3 [43]=3 [0]=3 [54]=3 [47]=3 [48]=3)"
+    literal_transitions[6]="([56]=3 [19]=3 [36]=3 [44]=3 [45]=3)"
+    literal_transitions[7]="([49]=3 [63]=3 [25]=3 [17]=3)"
+    literal_transitions[8]="([21]=3 [43]=3 [0]=3 [55]=3 [47]=3 [48]=3)"
     literal_transitions[9]="([40]=3)"
-    literal_transitions[10]="([18]=3)"
-    literal_transitions[11]="([39]=3 [56]=3)"
+    literal_transitions[10]="([20]=3)"
+    literal_transitions[11]="([39]=3 [57]=3)"
     literal_transitions[12]="([41]=3 [32]=19)"
-    literal_transitions[13]="([9]=3 [28]=3 [59]=3 [31]=3)"
-    literal_transitions[14]="([15]=3 [50]=3 [4]=3 [13]=3)"
-    literal_transitions[15]="([57]=3 [51]=3)"
+    literal_transitions[13]="([9]=3 [28]=3 [60]=3 [31]=3)"
+    literal_transitions[14]="([16]=3 [51]=3 [4]=3 [13]=3)"
+    literal_transitions[15]="([58]=3 [52]=3)"
     literal_transitions[16]="([2]=17)"
-    literal_transitions[17]="([69]=3)"
-    literal_transitions[18]="([20]=3 [11]=3 [46]=3 [64]=3)"
-    literal_transitions[19]="([17]=3 [27]=3 [26]=3 [8]=3)"
-    literal_transitions[20]="([42]=3 [30]=3 [52]=3 [68]=3)"
+    literal_transitions[17]="([70]=3)"
+    literal_transitions[18]="([15]=3 [11]=3 [46]=3 [65]=3)"
+    literal_transitions[19]="([18]=3 [27]=3 [26]=3 [8]=3)"
+    literal_transitions[20]="([42]=3 [30]=3 [53]=3 [69]=3)"
 
     declare -A match_anything_transitions
     match_anything_transitions=([0]=2 [2]=16)
