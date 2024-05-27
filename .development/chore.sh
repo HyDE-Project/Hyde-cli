94f67a0db5c96c2073e72d2e6d38d3f42999b591
kRHYME7 <alampayk@gmail.com>
2024-05-27 12:30:21 +0800

    fix:  Tries to have a local version of hyde

--- .development/chore.sh
+++ .development/chore.sh
@@ -3,3 +3,12 @@
 WORKSPACE=$(dirname $(dirname $(realpath ${0})))
 
 git describe --tags --abbrev | cut -d'-' -f1 | sed 's/^v//' > ${WORKSPACE}/version.txt && echo "[generated] version.txt "
+
+echo -e "\n"
+
+Hyde man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' > ${WORKSPACE}/USAGE && echo [generated] Hyde Usage
+
+echo -e "\n\n\n" >> ${WORKSPACE}/USAGE
+Hyde-tool man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g'   >> ${WORKSPACE}/USAGE && echo [generated] Hyde-tool Usage
+
+${WORKSPACE}/shell-completions/complgen.sh  
