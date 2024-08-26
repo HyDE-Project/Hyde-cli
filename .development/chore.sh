4c6a16a6691b61790afc91e4b65b2aa9b6baba28
kRHYME7 <alampayk@gmail.com>
2024-08-26 11:25:08 +0800

    chore: some Readme things

--- .development/chore.sh
+++ .development/chore.sh
@@ -8,8 +8,8 @@ git describe --tags --abbrev | cut -d'-' -f1 | sed 's/^v//' > ${WORKSPACE}/versi
 
 echo -e "\n"
 
-echo -e '```' >> ${WORKSPACE}/USAGE.md
-Hyde man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'     > ${WORKSPACE}/USAGE.md && echo [generated] Hyde Usage.md
+echo -e '```' > ${WORKSPACE}/USAGE.md
+Hyde man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'     >> ${WORKSPACE}/USAGE.md && echo [generated] Hyde Usage.md
 
 echo -e "\n\n\n" >> ${WORKSPACE}/USAGE.md
 Hyde-tool man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'      >> ${WORKSPACE}/USAGE.md && echo [generated] Hyde-tool Usage.md
