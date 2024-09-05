34882b5ee6f243009b7df148ccffa57a94e79eed
kRHYME7 <alampayk@gmail.com>
2024-09-05 11:15:23 +0800

    fix(Hyde man): cache the man page to avoid slow down

--- .development/chore.sh
+++ .development/chore.sh
@@ -9,10 +9,10 @@ git describe --tags --abbrev | cut -d'-' -f1 | sed 's/^v//' > ${WORKSPACE}/versi
 echo -e "\n"
 
 echo -e '```' > ${WORKSPACE}/USAGE.md
-Hyde man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'     >> ${WORKSPACE}/USAGE.md && echo [generated] Hyde Usage.md
+Hyde man --reload | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'     >> ${WORKSPACE}/USAGE.md && echo [generated] Hyde Usage.md
 
 echo -e "\n\n\n" >> ${WORKSPACE}/USAGE.md
-Hyde-tool man | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'      >> ${WORKSPACE}/USAGE.md && echo [generated] Hyde-tool Usage.md
+Hyde-tool man --reload | sed '/^Hyde Clone Directory:/d' | sed '/Hyde-cli Version/d' |  sed 's/\x1b\[[0-9;]*m//g' |  sed 's/ﯦ /💡/g' | sed 's/﬌/↪️/g'      >> ${WORKSPACE}/USAGE.md && echo [generated] Hyde-tool Usage.md
 echo -e '```' >> ${WORKSPACE}/USAGE.md
 
 ${WORKSPACE}/shell-completions/complgen.sh  
